ccfacaaa30390c440b2a204e3af3f007eea56fd3
ksszabla <ksszabla@amazon.com>
2024-09-26 15:10:17 +0100

    Updated database configuration

--- config.py
+++ config.py
@@ -6,6 +6,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'instance', 'site.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://postgresql_sea_ksszabla_user:xZR9KMnmlwUga1Z1aXguBr3H54zJSv8n@dpg-crqmcqdsvqrc73d1rsbg-a.frankfurt-postgres.render.com/postgresql_sea_ksszabla')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
